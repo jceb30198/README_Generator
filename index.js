b3d64977ea08b819b5e14eedfd673808343e7d59
Jack Ceballos <jackrceballos@gmail.com>
2020-12-05 18:06:38 -0500

    Array of questions added and now time to move on to the functions

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-const inquirer = require('inquirer');
+// Framework and File system constants
+const inquirer = require("inquirer");
+const fs = require("fs")
+
+// Array of prompts for the user to answer
+const promptArr = [
+    {
+        type: "input",
+        name: "title",
+        message: "Title of the project:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation instructions for the project:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Use of application:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Licenses for the project:",
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Contributors of the project:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Tests done on the project:",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "GitHub username:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Email used for GitHub:",
+    }
+]
+
+// Function to write the markdown file
