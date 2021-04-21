7782f7d43aebf6938a08ffc029785be9c35433ff
Jack Ceballos <jackrceballos@gmail.com>
2021-04-20 21:32:50 -0400

    Finally finished the template generator

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # README_Generator
+
+## Description:
+This command line application can be used to create a README.md file for any project that you may be working on!
+
+## Installation: 
+Clone the repository, then using the git bash terminal type, "node index" after press the Enter key. Finally answer all the questions and the template will generate the Markdown file.
+
+## Usage:
+Help others make easy work when it comes to creating their README's.
+
+## License:
+N/A
+
+## Contributors:
+Jack Ceballos
+
+## Tests:
+N/A
+
+## Personal: 
+- GitHub: jceb30198
+- Email: jack30198@gmail.com
--- index.js
+++ index.js
@@ -4,56 +4,84 @@ const fs = require("fs")
 
 // Array of prompts for the user to answer
 const promptArr = [
-    {
-        type: "input",
-        name: "title",
-        message: "Title of the project:",
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Describe the project:",
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Installation instructions for the project:",
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Use of application:",
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Licenses for the project:",
-    },
-    {
-        type: "input",
-        name: "contributions",
-        message: "Contributors of the project:",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Tests done on the project:",
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "GitHub username:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Email used for GitHub:",
-    }
-]
+  {
+    type: "input",
+    name: "title",
+    message: "Title of the project:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe the project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation instructions for the project:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Use of application:",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Licenses for the project:",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "Contributors of the project:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Tests done on the project:",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email used for GitHub:",
+  }
+];
 
-// Function to write the markdown file
-function writeFunc(file, responses) {
-    fs.writeFile(file, responses);
-    console.log("Success");
-}
+inquirer.prompt(promptArr).then((res) => {
+  const template = `
+  # ${res.title}
 
+  ## Description:
+  ${res.description}
+
+  ## Installation: 
+  ${res.installation}
+
+  ## Usage:
+  ${res.usage}
+
+  ## License:
+  ${res.license}
+
+  ## Contributors:
+  ${res.contributions}
+
+  ## Tests:
+  ${res.tests}
+
+  ## Personal: 
+  - GitHub: ${res.username}
+  - Email: ${res.email}`;
+
+  fs.writeFile('README.md', template, (err) => {
+    if(err) {
+      throw err
+    } else {
+      console.log('Template Generated!')
+    };
+  })
+});
